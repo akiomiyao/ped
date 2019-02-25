b949922a44d5c9c039144c784a8b5a077246cf4e
akiomiyao <miyao@affrc.go.jp>
2019-02-25 15:09:23 +0900

    add subroutine for standalone machine

--- count.pl
+++ count.pl
@@ -11,6 +11,7 @@ $usage = '
      count.pl - create count of kmers from sort_uniq data. 
 
 e.g. qsub -v target=ERR194147,number=0001,tmpdir=/mnt/ssd count.pl
+  or perl count.pl SRR8181712
 
      target is name of target.
      number is number of split target file (e.g. 0001).
@@ -26,37 +27,61 @@ if($ENV{target} ne ""){
     $cwd       = $ENV{PBS_O_WORKDIR};
     $tmpdir    = $ENV{tmpdir};
     $workdir = "$cwd/$target";
+    &cluster;
+}elsif($ARGV[0] ne ""){
+    &standalone;
+    exit;
 }else{
     print $usage;
     exit;
 }
 
-if ($tmpdir eq ""){
-    $tmpdir = $workdir;
-}else{
-    if (-e "$tmpdir/$target"){
-	system("rm -r $tmpdir/$target");
+sub cluster{
+    if ($tmpdir eq ""){
+	$tmpdir = $workdir;
+    }else{
+	if (-e "$tmpdir/$target"){
+	    system("rm -r $tmpdir/$target");
+	}
+	system("mkdir $tmpdir/$target");
+	$tmpdir = "$tmpdir/$target";
     }
-    system("mkdir $tmpdir/$target");
-    $tmpdir = "$tmpdir/$target";
-}
 
-chdir $tmpdir;
-
-$input_file = "$target.sort_uniq.$number";
-
-if ($tmpdir ne $workdir){
-    system("cp $workdir/tmp/$input_file $tmpdir");
-}
-open(IN, "$input_file");
-open(OUT, "|sort -S 2G -T $tmpdir | uniq -c | awk '{print \$2 \"\t\" \$1}' | /usr/bin/perl $cwd/split_count.pl");
-while(<IN>){
-    chomp;
-    $length = length($_);
-    for ($i = 0; $i <= $length - 20; $i++){
-	$seq = substr($_, $i, 20);
-	if ($seq !~ /N/){
-	    print OUT "$seq\n";
+    chdir $tmpdir;
+    
+    $input_file = "$target.sort_uniq.$number";
+    
+    if ($tmpdir ne $workdir){
+	system("cp $workdir/tmp/$input_file $tmpdir");
+    }
+    open(IN, "$input_file");
+    open(OUT, "|sort -S 2G -T $tmpdir | uniq -c | awk '{print \$2 \"\t\" \$1}' | /usr/bin/perl $cwd/split_count.pl");
+    while(<IN>){
+	chomp;
+	$length = length($_);
+	for ($i = 0; $i <= $length - 20; $i++){
+	    $seq = substr($_, $i, 20);
+	    if ($seq !~ /N/){
+		print OUT "$seq\n";
+	    }
 	}
     }
+}
+
+sub standalone{
+   $target = $ARGV[0];
+   $input_file = $target . ".sort_uniq";
+
+   open(IN, "$target/$input_file");
+   open(OUT, "|sort -S 2G -T ./ | uniq -c | awk '{print \$2 \"\t\" \$1}' | /usr//bin/perl last_base_count.pl > $target/$target.lbc");
+   while(<IN>){
+       chomp;
+       $length = length($_);
+       for ($i = 0; $i <= $length - 20; $i++){
+	   $seq = substr($_, $i, 20);
+	   if ($seq !~ /N/){
+	       print OUT "$seq\n";
+	   }
+       }
+   }
 }
--- last_base_count.pl
+++ last_base_count.pl
@@ -19,52 +19,80 @@ Author: Akio Miyao <miyao@affrc.go.jp>
 
 ';
 
+if ($ARGV[0] ne ""){
+    print $usage;
+    exit;
+}
+
 if($ENV{target} ne ""){
     $target    = $ENV{target};
     $tag       = $ENV{tag};
     $cwd       = $ENV{PBS_O_WORKDIR};
     $tmpdir    = $ENV{tmpdir};
     $workdir = "$cwd/$target";
+    &cluster;
 }else{
-    print $usage;
-    exit;
+    &standalone;
 }
 
-if ($tmpdir eq ""){
-    $tmpdir = $workdir;
-}else{
-    system("mkdir $tmpdir/$target");
-    $tmpdir = "$tmpdir/$target";
-    system("cp $workdir/$target.count.$tag.gz $tmpdir");
-}
-
-chdir $tmpdir;
-
-$output_file = "$target.lbc.$tag";
-
-open(IN, "/usr/bin/zcat $target.count.$tag.gz|");
-open(OUT, "> $output_file");
-while(<IN>){
-    chomp;
-    ($seq, $count) = split;
-    $tag = substr($seq, 0, 19);
-    $nuc = substr($seq, 19, 1);
-
-    if ($tag ne $prev and $prev ne ""){
-        print OUT "$prev\t$A\t$C\t$G\t$T\n";
-        $A = 0;
-        $C = 0;
-        $G = 0;
-        $T = 0;
+sub cluster{
+    if ($tmpdir eq ""){
+	$tmpdir = $workdir;
+    }else{
+	system("mkdir $tmpdir/$target");
+	$tmpdir = "$tmpdir/$target";
+	system("cp $workdir/$target.count.$tag.gz $tmpdir");
     }
-    $$nuc = $count;
-
-    $prev = $tag;
+    
+    chdir $tmpdir;
+    
+    $output_file = "$target.lbc.$tag";
+    
+    open(IN, "/usr/bin/zcat $target.count.$tag.gz|");
+    open(OUT, "> $output_file");
+    while(<IN>){
+	chomp;
+	($seq, $count) = split;
+	$tag = substr($seq, 0, 19);
+	$nuc = substr($seq, 19, 1);
+	
+	if ($tag ne $prev and $prev ne ""){
+	    print OUT "$prev\t$A\t$C\t$G\t$T\n";
+	    $A = 0;
+	    $C = 0;
+	    $G = 0;
+	    $T = 0;
+	}
+	$$nuc = $count;
+	
+	$prev = $tag;
+    }
+    print OUT "$prev\t$A\t$C\t$G\t$T\n";
+    close(OUT);
+    if ($tmpdir eq $workdir){
+	system("gzip $output_file");
+    }else{
+	system("gzip $output_file && cp $output_file.gz $workdir && rm -r $tmpdir");
+    }
 }
-print OUT "$prev\t$A\t$C\t$G\t$T\n";
-close(OUT);
-if ($tmpdir eq $workdir){
-    system("gzip $output_file");
-}else{
-    system("gzip $output_file && cp $output_file.gz $workdir && rm -r $tmpdir");
+
+sub standalone{
+    while(<>){
+	chomp;
+	($seq, $count) = split;
+	$tag = substr($seq, 0, 19);
+	$nuc = substr($seq, 19, 1);
+	
+	if ($tag ne $prev and $prev ne ""){
+	    print "$prev\t$A\t$C\t$G\t$T\n";
+	    $A = 0;
+	    $C = 0;
+	    $G = 0;
+	    $T = 0;
+	}
+	$$nuc = $count;
+	
+	$prev = $tag;
+    }
+    print "$prev\t$A\t$C\t$G\t$T\n";
 }
