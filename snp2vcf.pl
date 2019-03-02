e6e05f9aff1ecf638f2140134d62b45e183b2fc0
akiomiyao <miyao@affrc.go.jp>
2019-03-02 15:10:53 +0900

    Update for kmer result.

--- snp2vcf.pl
+++ snp2vcf.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl
 #
 # This file is a part of PED; a script for Polymorphic Edge Detection.
 #
@@ -8,23 +8,50 @@
 #
 
 $target = $ARGV[0];
+$type   = $ARGV[1];
 
 if ($target eq ""){
-    print "e.g. perl snp2vcf.pl SRR5886855
+    print "
+e.g. perl snp2vcf.pl SRR5886855
+     perl snp2vcf.pl SRR5886855 kmer
+
 SRR5886855.snp.verify files in target directoy will be converted to SRR5886855.snp.vcf.
+
+Default input file is result of bidirectional method.
+For converting result of kmer method, option 'kmer' at 2nd argument is required.
 " ;
     exit;
 }
 
-open(IN, "cat $target/$target.snp.verify.*|");
-open(OUT, "> $target/$target.snp.vcf");
-
+if ($type eq "kmer"){
+    open(IN, "cat $target/$target.kmer.verify.*|");
+    open(OUT, "> $target/$target.kmer.vcf");
+}else{
+    open(IN, "cat $target/$target.snp.verify.*|");
+    open(OUT, "> $target/$target.snp.vcf");
+}
+print OUT "##fileformat=VCFv4.2
+##FILTER=<ID=PASS,Description=\"All filters passed\">
+##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Approximate read depth)\">
+##INFO=<ID=GT,Number=1,Type=String,Description=\"Genotype\">
+##FORMAT=<ID=DP,Number=1,Type=Integer,Description=\"Read depth\">
+##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">
+#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t$target\n";
 while(<IN>){
     chomp;
-    @row = split;
-    if (/M/){
-	print OUT "chr$row[0]\t$row[1]\t.\t$row[2]\t$row[3]\t0\tPASS\tDP=$row[8];GT=1/1\n";
-    }elsif(/H/){
-	print OUT "chr$row[0]\t$row[1]\t.\t$row[2]\t$row[3]\t0\tPASS\tDP=$row[8];GT=0/1\n";
+    @row = split('\t', $_);
+    $dp = $row[$#row] -1;
+    if ($type eq "kmer"){
+ 	if (/M/){
+	    print OUT "chr$row[0]\t$row[1]\t.\t$row[4]\t$row[5]\t1000\tPASS\tDP=$row[8];GT=1/1\tDP:GT\t$row[8]:1/1\n";
+	}elsif(/H/){
+	    print OUT "chr$row[0]\t$row[1]\t.\t$row[4]\t$row[5]\t1000\tPASS\tDP=$row[8];GT=0/1\tDP:GT\t$row[8]:1/0\n";
+	}
+   }else{
+	if (/M/){
+	    print OUT "chr$row[0]\t$row[1]\t.\t$row[2]\t$row[3]\t1000\tPASS\tDP=$row[8];GT=1/1\tDP:GT\t$row[8]:1/1\n";
+	}elsif(/H/){
+	    print OUT "chr$row[0]\t$row[1]\t.\t$row[2]\t$row[3]\t1000\tPASS\tDP=$row[8];GT=0/1\tDP:GT\t$row[8]:1/0\n";
+	}
     }
 }
