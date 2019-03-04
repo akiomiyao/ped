376ecad321f9a77da241dd0e99a08ab62b2f9e69
akiomiyao <miyao@affrc.go.jp>
2019-03-04 16:56:10 +0900

    Add a demonstation script for bidirectinal alignment method.

--- demo_bidirectional.pl
+++ demo_bidirectional.pl
@@ -10,40 +10,55 @@
 $usage = '
      demo_bidirectional.pl - demonstration of bidirectional alignment method. 
 
-e.g. perl demo_bidirectional.pl
+e.g. perl demo_bidirectional.pl accession
+     perl demo_bidirectional.pl SRR8181712
+
+     This demonstration script is for sequence of Arabidopsis thaliana.
 
 Author: Akio Miyao <miyao@affrc.go.jp>
 
 ';
 
-if ($ARGV[0] ne ""){
+if ($ARGV[0] eq ""){
     print $usage;
     exit;
 }
 
-chdir "TAIR10";
-report("Making reference data.");
-system("/usr/bin/sh setup.sh");
+$target = $ARGV[0];
 
-chdir "..";
+if (! -e "TAIR10/TAIR10.sort_uniq"){
+    chdir "TAIR10";
+    report("Making reference data.");
+    system("/usr/bin/sh setup.sh");
+    chdir "..";
+}
 
-report("Downloading fastq files of SRR8181712.");
-system("perl download.pl SRR8181712");
-report("Making sort_uniq sequence of SRR8181712.");
-system("perl sort_uniq.pl SRR8181712");
-report("Aligning of SRR8181712 sequence to reference genome.");
-system("perl align.pl SRR8181712 TAIR10 5");
+if (! -e "$target/$target.sort_uniq"){
+    report("Downloading fastq files of $target.");
+    system("perl download.pl $target");
+    report("Making sort_uniq sequence of $target.");
+    system("perl sort_uniq.pl $target");
+}
+exit;
+report("Aligning of $target sequence to reference genome.");
+system("perl align.pl $target TAIR10 5");
 report("Splitting alignment of SNP.");
-system("perl split_snp.pl SRR8181712");
+system("perl split_snp.pl $target");
 report("Splitting alignment of Indel.");
-system("perl split_indel.pl SRR8181712");
-report("Verifying SNPs.");
-system("perl verify_snp.pl SRR8181712 default TAIR10 01 bi");
+system("perl split_indel.pl $target");
+opendir(DIR, $target);
+while(readdir(DIR)){
+    if (/$target.snp.[0-9][0-9]$/){
+	$number = (split('\.', $_))[2];
+	report("Verifying SNPs. $number");
+	system("perl verify_snp.pl $target default TAIR10 $number bi");
+    }
+}
 report("Verifying Indels.");
-system("perl verify_indel.pl SRR8181712 default TAIR10 01 bi");
+system("perl verify_indel.pl $target default TAIR10 01 bi");
 report("Making vcf file of SNP");
-system("perl snp2vcf.pl SRR8181712");
-system("mv SRR8181712.indel.verify.01 SRR8181712.indel");
+system("perl snp2vcf.pl $target");
+system("mv $target.indel.verify.01 $target.indel");
 report("Complete.");
 
 sub report{
