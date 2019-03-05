050c731617828f7187e2f254f8337430b8b137a9
akiomiyao <miyao@affrc.go.jp>
2019-03-05 11:49:00 +0900

    Add WBcel235

---
+++ WBcel235/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# This file is a part of PED; a script for Polymorphic Edge Detection.
+#
+# Copyright (C) 2017 National Agriculture and Food Research Organization. All Rights Reserved.
+#
+# License: refer to https://github.com/akiomiyao/ped
+#
+
+wget ftp://ftp.ensembl.org/pub/release-95/fasta/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz
+echo Making chromosome data.
+perl mkChr.pl
+echo Making 20mer reference.
+perl mk20.pl
+echo Making control sort_uniq file.
+perl mkControlRead.pl
+echo Done.
