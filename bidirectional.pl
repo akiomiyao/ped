84b8c93b5215202a4ab1a0618f45052b8a9b2496
Akio Miyao <miyao@affrc.go.jp>
2019-03-15 18:37:39 +0900

    Update bidirectional.pl

--- bidirectional.pl
+++ bidirectional.pl
@@ -100,7 +100,7 @@ closedir(DIR);
 report("Making vcf file of SNP");
 system("perl snp2vcf.pl $target");
 system("cat $target/$target.indel.verify.* > $target/$target.indel && rm $target/$target.indel.verify.* $target/$target.indel.??");
-system("cat $target/$target.snp.verify.* > $target/$target.snp && rm $target/$target.snp.verify.* $target/$target.snp.??");
+system("cat $target/$target.snp.verify.* > $target/$target.bi.snp && rm $target/$target.snp.verify.* $target/$target.snp.??");
 report("bidirectional.pl complete.");
 
 sub report{
