7e4e51c32fe0b6d0aabf91cc09f1b5a46baaa5bd
akiomiyao <miyao@affrc.go.jp>
2019-04-05 19:39:55 +0900

    Update mkref.pl

--- mkref.pl
+++ mkref.pl
@@ -101,22 +101,48 @@ sub mkChr{
     my $file = $file[$#file];
     my $i = 0;
     print "Making chromosome file\n";
-    open(IN, "zcat $file|");
-    while(<IN>){
-	chomp;
-	if (/^>/){
-	    close(OUT);
-	    $ref = "chr" . $chr[$i];
-	    last if $chr[$i] eq "";
-	    $i++;
-	    open(OUT, "> $ref");
-	}else{
-	    y/a-z/A-Z/;
-	    print OUT;
+    if ($target eq "hg38"){
+	open(IN, "zcat $file|");
+	while(<IN>){
+	    chomp;
+	    if (/^>/){
+		close(OUT);
+		$ref = (split)[0];
+		$ref =~ s/>//;
+		if ($ref =~ /\_/){
+		    $flag = 1;
+		}else{
+		    $flag = 0;
+		    $ref =~ s/chr//i;
+		    $ref += 0 if $ref =~ /^[0-9]*$/;
+		    $ref = "chr$ref";
+		    open(OUT, "> $ref");
+		}
+	    }elsif(! $flag){
+		y/a-z/A-Z/;
+		print OUT;
+	    }
 	}
+	close(IN);
+	close(OUT);
+    }else{	
+	open(IN, "zcat $file|");
+	while(<IN>){
+	    chomp;
+	    if (/^>/){
+		close(OUT);
+		$ref = "chr" . $chr[$i];
+		last if $chr[$i] eq "";
+		$i++;
+		open(OUT, "> $ref");
+	    }else{
+		y/a-z/A-Z/;
+		print OUT;
+	    }
+	}
+	close(IN);
+	close(OUT);
     }
-    close(IN);
-    close(OUT);
 }
 
 sub mk20{
