a31b4ba60a9591426d03e365aa0089478aec5534
akiomiyao <miyao@affrc.go.jp>
2019-11-04 09:46:56 +0900

    update for bidirectional.pl

--- bidirectional.pl
+++ bidirectional.pl
@@ -108,7 +108,7 @@ foreach(sort readdir(DIR)){
 closedir(DIR);
 
 report("Making vcf file of SNP");
-system("cat $target/$target.indel.verify.* > $target/$target.indel && rm $target/$target.indel.verify.* $target/$target.indel.??");
-system("cat $target/$target.snp.verify.* > $target/$target.bi.snp && rm $target/$target.snp $target/$target.snp.verify.* $target/$target.snp.??");
+system("cat $target/$target.indel.verify.* > $target/$target.indel && rm $target/$target.indel.verify.* $target/$target.indel.sort $target/$target.indel.??");
+system("cat $target/$target.snp.verify.* > $target/$target.bi.snp && rm $target/$target.snp.verify.*");
 system("perl snp2vcf.pl $target");
 report("bidirectional.pl complete.");
