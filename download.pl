67f3cf763d737101f2fd62c8dbf4dda48d89ecdc
Akio Miyao <miyao@affrc.go.jp>
2020-01-07 16:59:49 +0900

    Update download.pl

--- download.pl
+++ download.pl
@@ -10,19 +10,41 @@
 $usage = '
      download.pl - download fastq data from SRA of NCBI and setup directory. 
 
-e.g. perl download.pl ACCESSION
-     perl download.pl ERR194147
+e.g. perl download.pl ACCESSION Working_directory
+     perl download.pl ERR194147 /mnt/ssd/data
+
+     or 
+
+     perl download.pl accession=ACCESSION,wd=Working_directory
+     perl download.pl accession=ERR194147,wd=/mnt/ssd/data
+
+     Working_directory can be omitted.
 
 Author: Akio Miyao <miyao@affrc.go.jp>
 
 ';
 
-if ($ARGV[0] eq ""){
+if ($ARGV[0] =~ /accession/){
+    my @arg = split(',', $ARGV[0]);
+    foreach (sort @arg){
+	next if $_ eq "";
+	my ($name, $val) = split('=', $_);
+	$$name = $val;
+    }
+}elsif ($ARGV[0] eq ""){
     print "$usage";
     exit;
+}else{
+    $accession = $ARGV[0];
+    $wd = $ARGV[1];
 }
 
-system("mkdir $ARGV[0]");
-system("mkdir $ARGV[0]/read");
-chdir "$ARGV[0]/read";
-system("fastq-dump --split-files -A $ARGV[0]");
+if ($wd eq ""){
+    $wd = `pwd`;
+    chomp($wd);
+}
+
+system("mkdir $wd/$accession");
+system("mkdir $wd/$accession/read");
+chdir "$wd/$accession/read";
+system("fastq-dump --split-files -A $accession");
