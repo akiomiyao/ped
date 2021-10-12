6c141b0af98f209cfecabc701dd00ea4a5dbce3a
MIYAO Akio <akiomiyao@gmail.com>
2021-10-12 10:47:03 +0900

    More than two targets can be analyzed by compare.pl.

--- compare.pl
+++ compare.pl
@@ -6,86 +6,74 @@
 #
 # License: refer to https://github.com/akiomiyao/ped
 #
-# Author: MIYAO Akio <miyao@affrc.go.jp>
+# Author: Akio Miyao <miyao@affrc.go.jp>
 #
 
 $usage = "
  $0 - output joined genotype data from two selected targets with primer sequence
 
- e.g. perl $0 a=SRR14477896,b=SRR14477897
-      perl $0 a=SRR14477896,b=SRR14477897,type=sv
+ e.g. perl $0 target=SRR14477896:SRR14477897:SRR14477898
+      perl $0 target=SRR14477896:SRR14477897:SRR14477898,type=sv
       Space after comma is not allowed.
 
-      Alternatively,
-      perl $0 SRR14477896 SRR14477897
-      perl $0 SRR14477896 SRR14477897 sv
+      Result will be output to the standard output.
+      Genotypes following primer data will be appear with specified order of
+      target separated by ':'.
+      Number of targets can be changed.
 
-      Result will be output to the stanard output.
-
- Author: MIYAO Akio <miyao\@affrc.go.jp>
+ Author: Akio Miyao <miyao\@affrc.go.jp>
  Scripts: https://github.com/akiomiyao/ped
  Web page: https://akiomiyao.github.io/ped
  Docker: https://hub.docker.com/r/akiomiyao/ped  
+
 ";
 
 if ($ARGV[0] eq ""){
     print $usage;
     exit;
-}elsif($ARGV[0] =~ /,/){
+}elsif($ARGV[0] ne ""){
     foreach (split(",", $ARGV[0])){
 	($name, $value) = split("=", $_);
 	$$name = $value;
     }
-}else{
-    $a = $ARGV[0];
-    $b = $ARGV[1];
-    $type = $ARGV[2];
 }
+
+@target = split(':', $target);
 
 if($type eq "sv"){
-    open(IN, "$a/$a.sv.primer");
-    print "Chr\tPosition\tChr\tPosition\tType\tSize\t$a\t$b\tLeft_primer\tRight_primer\tSize\n";
+    print "Chr\tPosition\tChr\tPosition\tType\tSize\tLeft_primer\tRight_primer\tSize";
 }else{
-    open(IN, "$a/$a.bi.primer");
-    print "Chr\tPosition\tRef\tAlt\t$a\t$b\tLeft_primer\tRight_primer\tSize\tSequence around the mutation\n";
-}
-while(<IN>){
-    chomp;
-    @row = split;
-    $row[1] = "00000000000$row[1]";
-    $row[1] = substr($row[1], length($row[1])-11, 11);
-    if ($type eq "sv"){
-	$dat = "$row[0] $row[1] $row[2] $row[3] $row[5] $row[6]";
-	$t{$dat} = "$row[13]\t$row[14]\t$row[15]";
-	$a{$dat} = $row[11];
-    }else{
-	$dat = "$row[0] $row[1] $row[2] $row[3]";
-	$t{$dat} = "$row[9]\t$row[10]\t$row[11]\t$row[12]";
-	$a{$dat} = $row[8];
-    }
+    print "Chr\tPosition\tRef\tAlt\tLeft_primer\tRight_primer\tSize\tSequence around the mutation";
 }
 
-if($type eq "sv"){
-    open(IN, "$b/$b.sv.primer");
-}else{
-    open(IN, "$b/$b.bi.primer");
+
+foreach $name (@target){
+    print "\t$name";
 }
-while(<IN>){
-    chomp;
-    @row = split;
-    $row[1] = "00000000000$row[1]";
-    $row[1] = substr($row[1], length($row[1])-11, 11);
-    if ($type eq "sv"){
-	$dat = "$row[0] $row[1] $row[2] $row[3] $row[5] $row[6]";
-	$t{$dat} = "$row[13]\t$row[14]\t$row[15]";
-	$b{$dat} = $row[11];
+print "\n";
+
+foreach $name (@target){
+    if($type eq "sv"){
+	open(IN, "$name/$name.sv.primer");
     }else{
-	$dat = "$row[0] $row[1] $row[2] $row[3]";
-	$t{$dat} = "$row[9]\t$row[10]\t$row[11]\t$row[12]";
-	$b{$dat} = $row[8];
+	open(IN, "$name/$name.bi.primer");
+    }
+    while(<IN>){
+	chomp;
+	@row = split;
+	$row[1] = "00000000000$row[1]";
+	$row[1] = substr($row[1], length($row[1])-11, 11);
+	if ($type eq "sv"){
+	    $dat = "$row[0] $row[1] $row[2] $row[3] $row[5] $row[6]";
+	    $t{$dat} = "$row[13]\t$row[14]\t$row[15]";
+	    $$name{$dat} = $row[11];
+	}else{
+	    $dat = "$row[0] $row[1] $row[2] $row[3]";
+	    $t{$dat} = "$row[9]\t$row[10]\t$row[11]\t$row[12]";
+	    $$name{$dat} = $row[8];
+	}
     }
 }
-
 
 foreach (sort keys %t){
     @row = split;
@@ -95,11 +83,11 @@ foreach (sort keys %t){
     }else{
 	print "$row[0]\t$row[1]\t$row[2]\t$row[3]\t";
     }
-    $ga = $a{$_};
-    $ga = "W" if $ga eq "";
-    $gb = $b{$_};
-    $gb = "W" if $gb eq "";
-    print $ga;
-    print "\t$gb";
-    print "\t$t{$_}\n";
+    print "$t{$_}";
+    foreach $name (@target){
+	$gt = $$name{$_};
+	$gt = "W" if $gt eq "";
+	print "\t$gt";
+    }
+    print "\n";
 }
